0f141cea20676abb4026f964b76b24efe06e5060
Alex Luce <alexluce85@gmail.com>
2022-12-14 20:06:17 -0600

    added handle form submit function

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ let apiKey= "0e06341abdb059d8c70e1e1c71bad8f5"
 let button = document.querySelector("searchButton")
 let searchInput = document.querySelector("#searchInput")
 let storage = JSON.parse(localStorage.getItem("citySearchHistory"))
+
+function handleFormSubmit(event) {
+    event.preventDefault()
+    let userInput = searchInput.value.trim()
+    getWeatherdata(userInput)
+    storage.push(userInput)
+    localStorage.setItem("citySearchHistory",JSON.stringify(storage))
+    let listEl = document.createItem("li")
+    listEl.textContent = userInput
+    listEl.setAttribute("id", "searchHistory")
+    let showHistory = document.getElementById("showHistory")
+    showHistory.appendChild(listEl)
+    let liButton = document.getElementById("searchHistory")
+    liButton.onclick = searchHistorySearch
+
+}
