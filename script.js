701a557d0c24ceb6c54b4a04c8a96a7d78a8f5b9
Alex Luce <alexluce85@gmail.com>
2022-12-14 19:52:52 -0600

    finally got my api key

---
+++ script.js
@@ -0,0 +1,4 @@
+let apiKey= "0e06341abdb059d8c70e1e1c71bad8f5"
+let button = document.querySelector("searchButton")
+let searchInput = document.querySelector("#searchInput")
+let storage = JSON.parse(localStorage.getItem("citySearchHistory"))
